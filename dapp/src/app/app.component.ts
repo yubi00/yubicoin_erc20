74e6b161b88112e11b03b6e59b1a95069086d70d
YUBA RAJ KHADKA <ubrajkhadka@gmail.com>
2018-03-12 10:31:26 +1100

    signing transaction

--- dapp/src/app/app.component.ts
+++ dapp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {canBeNumber} from '../util/validation';
 
 const Web3 = require('web3');
 const contract = require('truffle-contract');
+const Tx = require('ethereumjs-tx');
 const yubicoinArtifacts = require('../../build/contracts/YubiCoin.json');
 
 @Component({
@@ -19,6 +20,8 @@ export class AppComponent {
 
   balance: number;
   totalsupply: number; 
+  etherbalance:number;
+  ethaccount: string;
   sendingAmount: number;
   recipientAddress: string;
   status: string;
@@ -27,6 +30,8 @@ export class AppComponent {
   constructor() {
     this.checkAndInstantiateWeb3();
     this.onReady();
+
+
   }
 
   checkAndInstantiateWeb3() {
@@ -65,6 +70,9 @@ export class AppComponent {
       this.account = this.accounts[0];
 
       this.initialSupply();
+      this.refreshBalance();
+      this.etherbalance = this.web3.fromWei(this.web3.eth.getBalance(this.account));
+      
     });
   }
 
@@ -76,7 +84,7 @@ export class AppComponent {
         return yubi.totalSupply.call();
       })
       .then((value) => {
-        this.totalsupply = value.toString(10);
+        this.totalsupply = this.web3.fromWei(value, 'ether').toNumber();
       })
       .catch((e) => {
         console.log(e);
@@ -94,7 +102,8 @@ export class AppComponent {
         });
       })
       .then((value) => {
-        this.balance = value;
+        this.balance = this.web3.fromWei(value, 'ether').toNumber();
+        console.log("token balance: "+this.balance);
       })
       .catch((e) => {
         console.log(e);
@@ -102,10 +111,27 @@ export class AppComponent {
       });
   }
 
-
-
   setStatus(message: string) {
     this.status = message;
+  }
+
+  waitForTransactionReceipt(hash: string)
+  {
+    console.log('waiting for contract to be mined');
+    const receipt = this.web3.eth.getTransactionReceipt(hash);
+    // If no receipt, try again in 1s
+    if (receipt == null) {
+        setTimeout(() => {
+            this.waitForTransactionReceipt(hash);
+        }, 1000);
+    } else {
+        // The transaction was mined, we can retrieve the contract address
+        console.log(receipt);
+        
+        console.log('Transaction hash: '+receipt.transactionHash);
+        console.log('contract address: ' + receipt.contractAddress);
+       
+    }
   }
 
   sendCoin() {
@@ -122,10 +148,60 @@ export class AppComponent {
        console.log("abi: "+contractabi);
        var contractaddress = yubi.address; 
        console.log("contract address: "+yubi.address);
-       var coinbase = '0x45aB33Ae8Ce087dee888191963785c09A5468f83';
-       console.log("coinbase acoount :"+coinbase);
-
+       var ethcontract = this.web3.eth.contract(contractabi).at(contractaddress);
        
+       var noncevalue = this.web3.eth.getTransactionCount(this.account);
+       console.log("nonce : "+ noncevalue);
+
+       var mydata = this.web3.sha3('transfer(address,uint)');
+       var mydatahex = mydata.substr(0,10);
+       console.log("My data: "+mydata); 
+       var mydata_2 = ethcontract.transfer.getData(receiver, amount, {from: this.account});
+       console.log("mYdata2: "+mydata_2);
+
+       var rawTx = {
+        "from": this.account,
+        "nonce": this.web3.toHex(noncevalue),
+        "gasPrice": this.web3.toHex(this.web3.eth.gasPrice),
+        "gasLimit":this.web3.toHex(300000),
+        "to": receiver,
+        "value": 0,
+        "data": mydatahex
+         };
+
+        console.log(rawTx);
+
+
+        
+        var privateKeybuff = new Buffer('6cf3addcb9f44801dfd55813cd278b6470f99137a4a8d43b04dd09f9bbf3c2df', 'hex');
+
+        
+        var tx = new Tx(rawTx);
+        console.log("tx: "+tx);
+        tx.sign(privateKeybuff);
+        if (tx.verifySignature()) {
+        console.log('Signature Checks out!' + tx);
+        }
+
+        var serializedTx = tx.serialize();
+        console.log("Serializetx:" +"0x"+serializedTx.toString('hex'));
+        var rawtxn = "0x"+serializedTx.toString('hex');
+
+        
+        this.web3.eth.sendRawTransaction(rawtxn, function(err, hash) {
+          if (!err){
+        // Log the tx, you can explore status manually with eth.getTransaction()
+            console.log('contract creation tx: ' + hash);
+        
+            // Wait for the transaction to be mined
+            this.waitForTransactionReceipt(hash);
+          }
+          else    
+          {
+            console.log(err);
+          }       
+        });
+
 
       })
       .then(() => {
